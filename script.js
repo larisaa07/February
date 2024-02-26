82750f97bc3845272b9598577ba5a2760374483b
larisaa07 <larisa_aveti@yahoo.com>
2024-02-26 13:47:55 -0500

    fixed stuff

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ function start (){
 }
 
 //need to fix this so that the if then statement works and the text changes if it is null; also make sure the text box disappears when the enter button is clicked
+
+//getting the computer choice
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+      return 'rock';
+      case 1:
+      return 'paper';
+      case 2:
+      return 'scissors'
+    }
+  }
