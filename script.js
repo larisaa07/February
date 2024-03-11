9dbc0330ed107ffa0e67de96e0b150a82542d05a
larisaa07 <larisa_aveti@yahoo.com>
2024-03-11 11:58:19 -0400

    almost done with the js. check over the ids to make sure they all match

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//universally defning
+let userScore=0
+let compScore=0
+let userName
+let userChoice
+let roundResult
+
 function start (){
     //get the name from the form
     let name = document.getElementById('userName').value;
@@ -10,9 +17,10 @@ function start (){
     }
 }
 
-//getting the computer choice
-function playGame(compChoice) {
-    let randomNumber = Math.floor(Math.random() * 3);
+//getting your choice and the computer choice
+function playGame(userSelection) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerSelection = choices[randomNumber = Math.floor(Math.random() * 3)];
     //the possible random choices:
     if (randomNumber === 0){
         compChoice = 'Rock'
@@ -21,8 +29,38 @@ function playGame(compChoice) {
     }else (randomNumber === 2){
         compChoice = 'Scissors'
     }
+
+    userChoice = userSelection
+    compChoice = computerSelection
+    
+    //who wins cases
+    if (userSelection === computerSelection){
+        roundResult = "It is a tie!"
+    }else if ((userSelection === 'rock' && computerSelection === 'scissors') || (userSelection === 'paper' && computerSelection === 'rock') || (userSelection === 'scissors' && computerSelection === 'paper')){
+        userScore++;
+        roundResult = "You win!!"
+
+        updateDisplayInfo()
+        isGameOver()
+    }else {
+        compScore++;
+        roundResult = "You lose!"
+
+        updateDisplayInfor()
+        isGameOver()
+    }
   }
 
-  //who wins cases
+  function isGameOver(){
+    if (userScore === 3){
+        console.log("You Win")
+        document.getElementById('result').innerText = `The game is over. ${userName} won with a final score of ${userScore} to ${compScore}.`
+    } else if (compScore === 3){
+        console.log("Computer Wins")
+        document.getElementById('result').innerText = `The game is over. The computer has won with a final score of ${compScore} to ${userScore}.`
+    }
+  }
 
-   
+  function updateDisplayInfo(){
+    
+  }
